c7b9b9d5e5d0d521254a72368047da6614e82eef
nilay994 <nilay_994@hotmail.com>
2020-02-05 16:24:41 +0100

    [uart] use state machine instead

---
+++ ESP32_firmware/Only_BS/Only_BS.h
@@ -0,0 +1,64 @@
+#define ESP_MAX_LEN 50
+#define DEVCHAR '1'
+
+
+// generic JEVOIS message structure
+struct esp_msg_t {
+  uint8_t type;
+  uint8_t id;
+  char str[ESP_MAX_LEN];
+};
+
+// decoder state
+enum esp_state {
+  ESP_SYNC = 0,
+  ESP_ID,
+  ESP_RX_MSG,
+  ESP_RX_OK,
+  ESP_RX_ERR
+};
+
+// similar to jevois struct
+struct esp_t {
+  enum esp_state state; // decoder state
+  char buf[ESP_MAX_LEN]; // temp buffer
+  uint8_t idx; // temp buffer index
+  struct esp_msg_t msg; // last decoded message
+  bool data_available; // new data to report
+};
+
+struct esp_t esp;
+
+
+// Beacon Packet buffer
+uint8_t packet[] = { 0x80, 0x00, 0x00, 0x00,
+                     /*Destination MAC*/
+                     /*4*/ 0xff, 0xff, 0xff, 0xff,
+                     /*8*/ 0xff, 0xff,
+                     /*Source MAC*/
+                     /*10*/0x01, 0x02, 0x03, 0x04,
+                     /*14*/0x05, 0x06,
+                     /*BSS ID*/
+                     /*16*/0x01, 0x02, 0x03, 0x04,
+                     /*20*/0x05, 0x06, 0xc0, 0x6c,
+                     /*Timestamp*/
+                     /*24*/0x83, 0x51, 0xf7, 0x8f,
+                     /*28*/0x0f, 0x00, 0x00, 0x00,
+                     /*32*/0x64, 0x00,/*Beacon interval*/
+                     /*34*/0x01, 0x04,/*Capability info*/
+                     /* SSID=ELEMENT ID> length> 28 bytes*/
+                     /*36*/0x00, 30,
+                     /*38*/'$', 'D', '0', DEVCHAR,
+                     /*42*/'M', 'A', 'V', 'L',
+                     /*46*/'A', 'B', '*', '*',
+                     /*50*/'*', '*', '*', '*',
+                     /*54*/'*', '*', '*', '*',
+                     /*58*/'*', '*', '*', '*',
+                     /*62*/'*', '*', '*', '*',
+                     /*66*/'*', '*',
+                     /*Supported rates*/
+                     /*68*/0x01, 0x08, 0x82, 0x84,
+                     /*72*/0x8b, 0x96, 0x24, 0x30,
+                     /*76*/0x48, 0x6c, 0x03, 0x01,
+                     /*80*/0x04
+                   };
--- ESP32_firmware/Only_BS/Only_BS.ino
+++ ESP32_firmware/Only_BS/Only_BS.ino
@@ -1,57 +1,11 @@
 #include <WiFi.h>
 #include "esp_wifi.h"
-
+#include "Only_BS.h"
 #include "wsled.h"
-
 
 esp_err_t esp_wifi_80211_tx(wifi_interface_t ifx, const void *buffer, int len, bool en_sys_seq);
 
 uint8_t channel = 0;
-#define DEVCHAR '1'
-
-int r;
-
-char buffer[28] = {'*', '*', '*', '*',
-                   '*', '*', '*', '*',
-                   '*', '*', '*', '*',
-                   '*', '*', '*', '*',
-                   '*', '*', '*', '*',
-                   '*', '*', '*', '*',
-                   '*', '*', '*', '*'
-                  };
-
-// Beacon Packet buffer
-uint8_t packet[] = { 0x80, 0x00, 0x00, 0x00,
-                     /*Destination MAC*/
-                     /*4*/ 0xff, 0xff, 0xff, 0xff,
-                     /*8*/ 0xff, 0xff,
-                     /*Source MAC*/
-                     /*10*/0x01, 0x02, 0x03, 0x04,
-                     /*14*/0x05, 0x06,
-                     /*BSS ID*/
-                     /*16*/0x01, 0x02, 0x03, 0x04,
-                     /*20*/0x05, 0x06, 0xc0, 0x6c,
-                     /*Timestamp*/
-                     /*24*/0x83, 0x51, 0xf7, 0x8f,
-                     /*28*/0x0f, 0x00, 0x00, 0x00,
-                     /*32*/0x64, 0x00,/*Beacon interval*/
-                     /*34*/0x01, 0x04,/*Capability info*/
-                     /* SSID=ELEMENT ID> length> 28 bytes*/
-                     /*36*/0x00, 30,
-                     /*38*/'(', DEVCHAR, 'E', 'S',
-                     /*42*/'P', '-', 'M', 'A',
-                     /*46*/'V', 'L', 'A', 'B',
-                     /*50*/DEVCHAR, '*', '*', '*',
-                     /*54*/'*', '*', '*', '*',
-                     /*58*/'*', '*', '*', '*',
-                     /*62*/'*', '*', '*', '*',
-                     /*66*/'*', '*',
-                     /*Supported rates*/
-                     /*68*/0x01, 0x08, 0x82, 0x84,
-                     /*72*/0x8b, 0x96, 0x24, 0x30,
-                     /*76*/0x48, 0x6c, 0x03, 0x01,
-                     /*80*/0x04
-                   };
 
 void random_mac()
 {
@@ -68,45 +22,49 @@ void random_mac()
 void scan(uint8_t ch, uint8_t Ts)
 {
   int numSsid = WiFi.scanNetworks(false, true, true, Ts, ch);
-
+  /* scan for other drone IDs */
   for (int j = 0; j < numSsid; j++) {
     String ssid = WiFi.SSID(j);
-    if (ssid.startsWith("(")) {
-      Serial.println(ssid);
+    /* print other drones location but with $ appended and CR/LN at the end of packet */
+    if (ssid.startsWith("$D")) {
+      
+      /* UART: esp2pprz
+       *  | Start Byte | Drone State (6*4) Bytes | End Byte (1 Byte) |
+       *  |------------|-------------------------|-------------------|
+       *  | $          | numbers                 | CR/LN             |
+       */
+      Serial.println('$' + ssid.substring(2));
     }
-
   }
 }
 
 void broadcastSSID()
 {
   random_mac();
-
   for (int c = 1; c < 15; c++) {
-
     packet[80] = c;
     esp_wifi_set_channel(c, WIFI_SECOND_CHAN_NONE);
     esp_wifi_80211_tx(WIFI_IF_AP, packet, 81, false);
-
     delay(1);
-  }//14 channels
+  } //14 channels
   delay(8);
 }
 
 void setup()
 {
+  // reset uart state machine 
+  esp.state = ESP_SYNC;
+  
   Serial.begin(115200);
   Serial.setTimeout(100);
   WiFi.mode(WIFI_AP_STA);
 
-  //Set channel
+  // Set channel
   channel = random(1, 14);
   esp_wifi_set_promiscuous(true);
   esp_wifi_set_max_tx_power(78);
 
-  
-
-  //Select external antenna
+  // Select external antenna
   pinMode(21, OUTPUT);
   digitalWrite(21, HIGH);
 
@@ -115,70 +73,161 @@ void setup()
   FastLED.setBrightness(32);
   leds[0] = CRGB::Black;
   FastLED.show();
-
+  
   delay(1000);
 
-  Serial.println("DEVICE: " + DEVCHAR);
+  // Serial.println("DEVICE: " + DEVCHAR);
 }
 
-
+// invoked every second
 int ctr = 0;
-
 void onesecondloop() {
   unsigned long curr_time = millis();
   static unsigned long prev_time = curr_time;
+  
+  esp_state curr_esp_state = esp.state;
+  static esp_state prev_esp_state = curr_esp_state;
+   
   if ((curr_time - prev_time) > 1000) {
     ctr = ctr + 1;
-    prev_time = curr_time;
+
     // toggle every second
     if (ctr % 2 == 0) {
-      if (DEVCHAR == '2') {
-        leds[0] = CRGB::Red;
-      }
-      if (DEVCHAR == '1') {
-        leds[0] = CRGB::Green;
-      }
+        if (prev_esp_state != curr_esp_state) {
+          leds[0] = CRGB::Red;
+        } else {
+          leds[0] = CRGB::Green;
+        }
+        
+    } else {
+        leds[0] = CRGB::Black;
     }
-    else {
-      leds[0] = CRGB::Black;
-    }
+    
     FastLED.show();
+    
+    prev_esp_state = curr_esp_state;
+    prev_time = curr_time;
+    
+    // Serial print current ssid every second
+    #ifdef DEBUG
+        // Used to synchronize UART communication
+        // Serial.println('>'); 
+        // Serial.write(17);//XON
+        for (int i = 38; i < 68; i++) {
+          Serial.print((char)packet[i]);
+        }
+        Serial.println("EOF");
+        // Serial.write(19);//XOFF
+    #endif
+    
+
   }
+}
+
+void parse_bytes(char c) {
+
+  /* UART: pprz2esp
+   *  | Start Byte | Drone State (6*4) Bytes | End Byte (1 Byte) |
+   *  |------------|-------------------------|-------------------|
+   *  | $          | numbers                 | CR/LN/*           |
+   */
+  
+  // NOTE: CR and LF are two seperate chars
+  
+  switch (esp.state) {
+    case ESP_SYNC: {
+      /* first char, sync string */
+      if (c == '$') {
+        esp.state = ESP_RX_MSG;
+      }
+    } break;
+    case ESP_RX_MSG: {  
+      /* not misra complaint to declare in case, but local-var */
+      static uint8_t byte_ctr = 0;
+      
+      /* if received terminate before string complete (atleast 3 bytes), trigger ERROR */
+      if ((c=='\0' || c=='\n' || c=='\r' || c=='*') && (byte_ctr < 3)) {
+        esp.state = ESP_RX_ERR;
+      }
+      
+      /* after receiving the msg, terminate ssid string */
+      if ((c=='\0' || c=='\n' || c=='\r' || c=='*') && (byte_ctr >= 3)) {
+        // TODO: no need to terminate ssid
+        esp.msg.str[byte_ctr] = '\0';
+        byte_ctr = 0;
+        /* received message, now switch state machine */
+        esp.state = ESP_RX_OK; 
+      }
+      
+      /* record ssid until full lat,long,alt are stored in esp.msg.str */
+      /* don't change state machine until str terminate char is received */
+      else {
+        esp.msg.str[byte_ctr] = c;
+        byte_ctr = byte_ctr + 1;
+      }
+    } break;
+    case ESP_RX_OK: {
+            char tmp_str[80] = {0};
+            #ifdef DEBUG
+            /* string is okay, print it out and reset the state machine */
+            sprintf(tmp_str, "esp.state: %d, esp.msg.str: %s\n", esp.state, esp.msg.str);
+            Serial.print(tmp_str);
+            #endif
+            // strncpy(&packet[42], esp.msg.str, 24);
+            for (uint8_t len=0; len<24; len++) {
+              packet[42+len] = esp.msg.str[len];  
+            }
+
+            /* reset state machine */
+            esp.state = ESP_SYNC;
+
+    } break;
+    case ESP_RX_ERR: {
+            char tmp_str[80] = {0};
+            #ifdef DEBUG 
+            sprintf(tmp_str, "ESP_RX_ERR: string terminated before drone info\n");
+            Serial.println(tmp_str);
+            #endif
+            
+            /* reset state machine, string terminated earlier than expected */
+            esp.state = ESP_SYNC;
+   } break;
+    default: {
+            esp.state = ESP_SYNC;
+    } break;
+    
+  } /* end switchcase */
 }
 
 
 void loop()
 {
   onesecondloop();
-  r = random(0, 9999);
 
-  // three digit alive packet, 4th as terminator
-  char eta[4] = {'0'};
+  // send to state machine
+  if (Serial.available() > 0) {
+    char incomingByte = Serial.read();
+    parse_bytes(incomingByte);
+  }
+      
+  int r = random(0, 9999);
 
   //50Tx,50Rx
   if (r < 6667) {
-
-    // //digitalWrite(15, HIGH);
-    // Serial.println('>'); //hotUsed to synchronize UART communication
-    // Serial.write(17);//XON
-
-    // alive packet
-    sprintf(eta, "%03d", ctr);
-    // Serial.println(eta);
-    packet[47] = eta[0];
-    packet[48] = eta[1];
-    packet[49] = eta[2];
-
-    int b = Serial.readBytesUntil('*', &packet[40], 26);
-    // Serial.write(19);//XOFF
-    broadcastSSID();//B
-
-  } else {
-    unsigned long start = micros();
-    //Serial.println('S');
+    broadcastSSID();
+  } 
+  else {
     scan(channel, 60); //S
-    unsigned long stop = micros();
-    //Serial.printf("s-dur: %u\r\n", stop - start);
   }
-
 }
+
+/*
+  // three digit alive packet, 4th as terminator (for testing outdoors)
+  char eta[4] = {'0'};
+  // alive packet
+  sprintf(eta, "%03d", ctr);
+  Serial.println(eta);
+  packet[47] = eta[0];
+  packet[48] = eta[1];
+  packet[49] = eta[2];
+*/
