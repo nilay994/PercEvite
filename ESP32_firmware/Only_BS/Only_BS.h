07e79c5e40a08e4d6be51402048468456d6ec635
nilay <nilay_994@hotmail.com>
2020-02-12 20:06:19 +0100

    [uart] bytestuff to hex

--- ESP32_firmware/Only_BS/Only_BS.h
+++ ESP32_firmware/Only_BS/Only_BS.h
@@ -1,35 +1,52 @@
-#define ESP_MAX_LEN 50
-#define DEVCHAR '1'
+#ifndef ONLY_BS_H
+#define ONLY_BS_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-// generic JEVOIS message structure
-struct esp_msg_t {
-  uint8_t type;
-  uint8_t id;
-  char str[ESP_MAX_LEN];
-};
+#define ESP_MAX_LEN 50 // lat,long,alt,bearing = 51 bytes max (28 currently)
+#define MAX_DRONES 2   // maximum drones in ESP32's range
+
+typedef enum {
+  ACK_FRAME = 0,
+  DATA_FRAME
+} frame_type_t;
+
 
 // decoder state
-enum esp_state {
+typedef enum {
   ESP_SYNC = 0,
-  ESP_ID,
-  ESP_OK_CHECK,
-  ESP_RX_MSG,
+  ESP_DRONE_INFO,
+  ESP_DRONE_DATA,
+  ESP_ERR_CHK,
   ESP_RX_OK,
   ESP_RX_ERR
-};
+} esp_state_t;
 
-// similar to jevois struct
-struct esp_t {
-  enum esp_state state; // decoder state
-  char buf[ESP_MAX_LEN]; // temp buffer
-  uint8_t idx; // temp buffer index
-  struct esp_msg_t msg; // last decoded message
-  bool data_available; // new data to report
-};
+typedef struct __attribute__((packed)) {
+  float x;
+  float y;
+  float z;
+} vec3f_t;
 
-struct esp_t esp;
+typedef struct __attribute__((packed)) {
+  vec3f_t pos;
+  float heading;
+  vec3f_t vel;
+} drone_data_t;
 
+typedef struct __attribute__((packed)) {
+  uint8_t drone_id;
+  uint8_t packet_type;
+  uint8_t packet_length;
+} drone_info_t;
+
+// encapsulated in $ and * 
+typedef struct __attribute__((packed)){
+  drone_info_t info; 
+  drone_data_t data;
+} uart_packet_t;
 
 // Beacon Packet buffer
 uint8_t packet[] = { 0x80, 0x00, 0x00, 0x00,
@@ -49,17 +66,24 @@ uint8_t packet[] = { 0x80, 0x00, 0x00, 0x00,
                      /*34*/0x01, 0x04,/*Capability info*/
                      /* SSID=ELEMENT ID> length> 28 bytes*/
                      /*36*/0x00, 30,
-                     /*38*/'$', 'D', '0', DEVCHAR,
-                     /*42*/'M', 'A', 'V', 'L',
-                     /*46*/'A', 'B', '*', '*',
-                     /*50*/'*', '*', '*', '*',
-                     /*54*/'*', '*', '*', '*',
-                     /*58*/'*', '*', '*', '*',
-                     /*62*/'*', '*', '*', '*',
-                     /*66*/'*', '*',
+                     /*38*/0x01, 0x01, 0x21, 0x0A,
+                     /*42*/0xd7, 0xc3, 0xbf, 0xf4,
+                     /*46*/0x1d, 0xad, 0xc3, 0x78,
+                     /*50*/0x3e, 0xb7, 0x46, 0x5c,
+                     /*54*/0x0f, 0xe2, 0xc3, 0x0a,
+                     /*58*/0xd7, 0xc3, 0xbf, 0xf4,
+                     /*62*/0x1d, 0xad, 0x78, 0x3e,
+                     /*66*/0xb7, 0x46,
                      /*Supported rates*/
                      /*68*/0x01, 0x08, 0x82, 0x84,
                      /*72*/0x8b, 0x96, 0x24, 0x30,
                      /*76*/0x48, 0x6c, 0x03, 0x01,
                      /*80*/0x04
                    };
+
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif
--- ESP32_firmware/Only_BS/Only_BS.ino
+++ ESP32_firmware/Only_BS/Only_BS.ino
@@ -3,9 +3,14 @@
 #include "Only_BS.h"
 #include "wsled.h"
 
-// #define DEBUG
+//#define DBG
 
-esp_err_t esp_wifi_80211_tx(wifi_interface_t ifx, const void *buffer, int len, bool en_sys_seq);
+// esp_err_t esp_wifi_80211_tx(wifi_interface_t ifx, const void *buffer, int len, bool en_sys_seq);
+
+drone_data_t dr_data[MAX_DRONES];
+uint8_t esp_state = ESP_SYNC;
+
+
 
 uint8_t channel = 0;
 
@@ -23,7 +28,8 @@ void random_mac()
 
 void scan(uint8_t ch, uint8_t Ts)
 {
-  int numSsid = WiFi.scanNetworks(false, true, true, Ts, ch);
+  // int numSsid = WiFi.scanNetworks(false, true, true, Ts, ch);
+  int numSsid = 0;
   /* scan for other drone IDs */
   for (int j = 0; j < numSsid; j++) {
     String ssid = WiFi.SSID(j);
@@ -52,10 +58,25 @@ void broadcastSSID()
   delay(8);
 }
 
+// init: clear all data for all drones
+static void clear_drone_status(void) {
+  for (uint8_t id = 0; id < MAX_DRONES; id++) {
+    // initialize at tropical waters of eastern Altanic ocean, facing the artic
+    dr_data[id].pos.x = 0;
+    dr_data[id].pos.y = 0;
+    dr_data[id].pos.z = 0;
+    dr_data[id].heading = 0;
+    dr_data[id].vel.x = 0;
+    dr_data[id].vel.y = 0;
+    dr_data[id].vel.z = 0;
+  }
+}
+
+
 void setup()
 {
   // reset uart state machine 
-  esp.state = ESP_SYNC;
+  esp_state = ESP_SYNC;
   
   Serial.begin(115200);
   Serial.setTimeout(100);
@@ -75,7 +96,8 @@ void setup()
   FastLED.setBrightness(32);
   leds[0] = CRGB::Green;
   FastLED.show();
-  
+
+  clear_drone_status();
   delay(1000);
 
   // Serial.println("DEVICE: " + DEVCHAR);
@@ -84,138 +106,212 @@ void setup()
 // invoked every second
 int ctr = 0;
 void onesecondloop() {
-  unsigned long curr_time = millis();
-  static unsigned long prev_time = curr_time;
-  
-  esp_state curr_esp_state = esp.state;
-  static esp_state prev_esp_state = curr_esp_state;
-   
-  if ((curr_time - prev_time) > 1000) {
-    ctr = ctr + 1;
-    
-    prev_esp_state = curr_esp_state;
-    prev_time = curr_time;
-    
-    // Serial print current ssid every second
-    #ifdef DEBUG
-        // Used to synchronize UART communication
-        // Serial.println('>'); 
-        // Serial.write(17);//XON
-        for (int i = 38; i < 68; i++) {
-          Serial.print((char)packet[i]);
-        }
-        Serial.println("EOF");
-        // Serial.write(19);//XOFF
-    #endif
-    
-
-  }
+//  unsigned long curr_time = millis();
+//  static unsigned long prev_time = curr_time;
+//  
+//  esp_state curr_esp_state = esp_state;
+//  static esp_state prev_esp_state = curr_esp_state;
+//   
+//  if ((curr_time - prev_time) > 1000) {
+//    ctr = ctr + 1;
+//    
+//    prev_esp_state = curr_esp_state;
+//    prev_time = curr_time;
+//    
+//    // Serial print current ssid every second
+//    #ifdef DEBUG
+//        // Used to synchronize UART communication
+//        // Serial.println('>'); 
+//        // Serial.write(17);//XON
+//        for (int i = 38; i < 68; i++) {
+//          Serial.print((char)packet[i]);
+//        }
+//        Serial.println("EOF");
+//        // Serial.write(19);//XOFF
+//    #endif
+//    
+//
+//  }
 }
 
-void parse_bytes(char c) {
+// rx: print struct received after checksum match
+static void print_drone_data_struct(drone_data_t *dat) {
+  char tmpstr[200] = {0};
+	sprintf(tmpstr, "dat->pos.x: %f, dat->pos.y: %f, dat->pos.z: %f\n"
+				 "dat->heading: %f\n"
+				 "dat->vel.x: %f, dat->vel.y: %f, dat->vel.z: %f\n",
+					dat->pos.x, dat->pos.y,	dat->pos.z,
+					dat->heading,
+					dat->vel.x,	dat->vel.y, dat->vel.z);
+  Serial.println(tmpstr);
+}
 
-  /* UART: pprz2esp
-   *  | Start Byte | Drone State (6*4) Bytes | End Byte (1 Byte) |
-   *  |------------|-------------------------|-------------------|
-   *  | $          | numbers                 | CR/LN/*           |
-   */
 
+// rx: receive drone_data in a string
+static void rx_struct(drone_data_t *dr_dat, uint8_t* buf) {
+	memcpy(dr_dat, buf, sizeof(drone_data_t));
+	#ifdef DBG
+	print_drone_data_struct(dr_dat);
+  #endif
+  // rx_success indicate
+  static bool toggle = 0;
+  toggle = !toggle;
+  if (toggle) {
+      leds[0] = CRGB::Red;
+  } else {
+      leds[0] = CRGB::Black;
+  }
+  FastLED.show(); 
+}
+
+
+// null terminated at the end is fine for memcpy
+uint8_t localbuf[ESP_MAX_LEN] = {0};
+
+void esp_parse(uint8_t c) {
   
   static uint8_t byte_ctr = 0;
-  // NOTE: CR and LF are two seperate chars
-  
-  switch (esp.state) {
+  static uint8_t drone_id = 0;
+  static uint8_t packet_length = 0;
+  static uint8_t packet_type = 0;
+  static uint8_t checksum = 0;
+  static uint8_t prev_char = 0;
+
+  #ifdef DBG
+  char tmstr[40] = {0};
+  sprintf(tmstr, "esp_state: %d, char rxed: 0x%02x\n", esp_state, c);
+  Serial.println(tmstr);
+  #endif
+
+  switch (esp_state) {
     case ESP_SYNC: {
+
       /* first char, sync string */
       if (c == '$') {
-        esp.state = ESP_OK_CHECK;
-      }
-    } break;
-    case ESP_OK_CHECK: {
-        /* check for OK string */
-        if ((c == 'O') && (byte_ctr == 0)) {
-          byte_ctr = byte_ctr + 1;
-          break;
-        } else if ((c == 'K') && (byte_ctr == 1)) {
-          static bool toggle = 0; 
-          if (toggle) {  
-            // blink red led
-            leds[0] = CRGB::Red;
-            FastLED.show();
-          } else {
-            leds[0] = CRGB::Green;
-            FastLED.show();
-          }
-          char tmp_str[80] = {0};
-          #ifdef DEBUG
-            /* string is okay, print it out and reset the state machine */
-            sprintf(tmp_str, "esp.state: %d, OKAY received\n", esp.state);
-            Serial.print(tmp_str);
-          #endif
-          toggle = !toggle;
-          byte_ctr = 0;
-          esp.state = ESP_SYNC;
-        } else {
-          byte_ctr = 0;
-          esp.state = ESP_RX_MSG;
-      }
-    } break;
-    case ESP_RX_MSG: {  
-      
-      /* if received terminate before string complete (atleast 3 bytes), trigger ERROR */
-      if (((c < 48) || (c > 57)) && (c!='.') && (c!= '-') && (byte_ctr < 10)) {
-        byte_ctr = 0;
-        esp.state = ESP_RX_ERR;
-      }
-      
-      /* after receiving the msg, terminate ssid string */
-      if ((c=='\0' || c=='\n' || c=='\r' || c=='*') && (byte_ctr >= 10)) {
-        // TODO: no need to terminate ssid
-        // esp.msg.str[byte_ctr] = '\0';
-        byte_ctr = 0;
-        /* received message, now switch state machine */
-        esp.state = ESP_RX_OK; 
-      }
-      
-      /* record ssid until full lat,long,alt are stored in esp.msg.str */
-      /* don't change state machine until str terminate char is received */
-      else {
-        esp.msg.str[byte_ctr] = c;
         byte_ctr = byte_ctr + 1;
       }
-    } break;
-    case ESP_RX_OK: {
-            char tmp_str[80] = {0};
-            #ifdef DEBUG
-            /* string is okay, print it out and reset the state machine */
-            sprintf(tmp_str, "esp.state: %d, esp.msg.str: %s\n", esp.state, esp.msg.str);
-            Serial.print(tmp_str);
-            #endif
-            // strncpy(&packet[42], esp.msg.str, 24);
-            for (uint8_t len=0; len<24; len++) {
-              packet[42+len] = esp.msg.str[len];  
-            }
 
-            /* reset state machine */
-            esp.state = ESP_SYNC;
+      /* second char: are you really the start of the packet? */
+      if ((byte_ctr == 1) && (c == 178)) {
+        byte_ctr = byte_ctr + 1;
+        esp_state = ESP_DRONE_INFO;
+      }
+    } break;
+
+    case ESP_DRONE_INFO: {
+      if (byte_ctr == 2) {
+        /* take note of packet length */
+        drone_id = c;
+        byte_ctr = byte_ctr + 1;
+      } else if (byte_ctr == 3) {
+        /* take note of packet type */
+        packet_type = c;
+        byte_ctr = byte_ctr + 1;
+      } else if (byte_ctr == 4) {
+        /* take note of drone ID */
+        packet_length = c;
+        byte_ctr = byte_ctr + 1;
+
+        // info frame populated!! 
+        // printf("[uart] packet_length: %d, packet_type: %d, drone_id: %d\n", packet_length, packet_type, drone_id);
+
+        if (packet_type == ACK_FRAME && packet_length == 4) {
+          // TODO: esp received ssid change signal and sent you ack, 
+          // indicate that on bool pprz esp ping? 
+          esp_state = ESP_RX_OK;
+          byte_ctr = 0;
+        }
+
+        /* packet length will always be shorter than padded struct, create some leeway */
+        else if ((packet_type == DATA_FRAME) && (packet_length >= (sizeof(drone_data_t)-5))) {
+          esp_state = ESP_DRONE_DATA;
+        } else if (packet_length > ESP_MAX_LEN) {
+          // printf("[uart-err] Packet unexpectedly long \n");
+          esp_state = ESP_RX_ERR;
+        }	else {
+          // do nothing?!
+        } 
+      } else {
+        // do nothing?!
+      }
+      
+    } break;
+
+    case ESP_DRONE_DATA: {
+      // TODO: remove when full checksum
+      uint8_t st_byte_pos = 5;
+
+      if (byte_ctr < packet_length) {
+        /* fill a localbuf and calculate local checksum */
+        localbuf[byte_ctr - st_byte_pos] = c;
+
+        checksum += localbuf[byte_ctr - st_byte_pos];
+        
+        byte_ctr = byte_ctr + 1;
+        // printf("byte_ctr: %d, idx: %d\n", byte_ctr, byte_ctr - st_byte_pos);		
+      }
+
+      /* after receiving the msg, terminate ssid string */
+      if (byte_ctr == packet_length) {
+        byte_ctr = 0;
+        esp_state = ESP_ERR_CHK;
+      }
+    } break;
+
+    case ESP_ERR_CHK: {
+      /* check if last packet matches your checksum */
+      if (c == checksum) {
+        // printf("[uart] checksum matched!\n");
+        esp_state = ESP_RX_OK;
+      }
+      else {
+        esp_state = ESP_RX_ERR;
+      }
+    } // no break statement required;
+
+    case ESP_RX_OK: {
+      #ifdef DBG
+      char tmstr1[50] = {0};
+      sprintf(tmstr1, "[uart] ESP_RX_OK\n");
+      Serial.println(tmstr1);
+      #endif
+
+      /* checksum matches, proceed to populate the struct */
+      rx_struct(&dr_data[drone_id], localbuf);
+      checksum = 0;
+
+      /* reset state machine */
+      esp_state = ESP_SYNC;
 
     } break;
     case ESP_RX_ERR: {
-            char tmp_str[80] = {0};
-            #ifdef DEBUG 
-            sprintf(tmp_str, "ESP_RX_ERR: string terminated before drone info\n");
-            Serial.println(tmp_str);
-            #endif
+            #ifdef DBG
+            char tmstr2[50] = {0};
+            sprintf(tmstr2, "[uart] ESP_RX_ERR\n");
+            Serial.println(tmstr2);
+            #endif 
             
+            byte_ctr = 0;
+            checksum = 0;
             /* reset state machine, string terminated earlier than expected */
-            esp.state = ESP_SYNC;
-   } break;
-    default: {
-            esp.state = ESP_SYNC;
+            esp_state = ESP_SYNC;
     } break;
-    
-  } /* end switchcase */
+    default: {
+            byte_ctr = 0;
+            esp_state = ESP_SYNC;
+    } break;
+  }
+
+  /* reset state machine asap, when start pattern is observed */
+  if (prev_char == '$' && c == 178) {
+    byte_ctr = 2;
+    esp_state = ESP_DRONE_INFO;
+  }
+
+  /* for start byte pattern check */
+  prev_char = c;
 }
+
 
 
 void loop()
@@ -225,7 +321,7 @@ void loop()
   // send to state machine
   if (Serial.available() > 0) {
     char incomingByte = Serial.read();
-    parse_bytes(incomingByte);
+    esp_parse((uint8_t) incomingByte);
   }
       
   int r = random(0, 9999);
@@ -238,14 +334,3 @@ void loop()
     scan(channel, 60); //S
   }
 }
-
-/*
-  // three digit alive packet, 4th as terminator (for testing outdoors)
-  char eta[4] = {'0'};
-  // alive packet
-  sprintf(eta, "%03d", ctr);
-  Serial.println(eta);
-  packet[47] = eta[0];
-  packet[48] = eta[1];
-  packet[49] = eta[2];
-*/
